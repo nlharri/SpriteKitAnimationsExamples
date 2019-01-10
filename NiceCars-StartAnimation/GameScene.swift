bf06f102ac0573178234551378eafc640319c286
Harri <nlharri@gmail.com>
2019-01-10 08:24:01 +0000

    initial commit

---
+++ NiceCars-StartAnimation/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//
+//
+//  Created by user913992 on 1/9/19.
+//  Copyright © 2019 nlharri. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    
+    override func didMove(to view: SKView) {
+        
+        // Get label node from scene and store it for use later
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
+    }
+}
